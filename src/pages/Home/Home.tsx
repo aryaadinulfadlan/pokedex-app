56b45c2e3bbe86fc1a38c2e54645328e42b00237
Arya Adinul Fadlan <64993961+aryaadinulfadlan@users.noreply.github.com>
2023-12-18 16:20:21 +0700

    navigate to prodcts page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,19 @@
 import { BtnBannerWrap, DemoButton, DescWrap, HomeContainer, ImgWrap, TryButton, WaIconWrapper } from "./HomeStyle"
 import Banner from '../../assets/banner.png'
 import { BsWhatsapp } from "react-icons/bs";
+import { useNavigate } from "react-router-dom"
 
 const HomePage = () => {
+  const navigate = useNavigate()
+  const goToProductsPage = () => navigate('/products')
   return (
     <HomeContainer>
       <DescWrap>
         <h3>the official pokemon website</h3>
         <p>The Pokédex section has a wealth of information on all the Pokémon creatures from the entire game series. On the main list pages you can see the various stats of each Pokémon. Click a Pokémon's name to see a detailed page with Pokédex data, descriptions from previous games, sprites, evolutions, moves and more!</p>
         <BtnBannerWrap>
-          <TryButton>try it free !</TryButton>
-          <DemoButton>App Demo</DemoButton>
+          <TryButton onClick={goToProductsPage}>try it free !</TryButton>
+          <DemoButton onClick={goToProductsPage}>App Demo</DemoButton>
         </BtnBannerWrap>
       </DescWrap>
       <ImgWrap>
